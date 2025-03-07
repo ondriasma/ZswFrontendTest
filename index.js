ab52605e2582dcc65900c1db0b59c04177f54e0f
ondriasma <ondriasma@protonmail.com>
2025-03-07 23:07:52 +0100

    Added Scrollbar, minor improvements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Happy developing ✨')
+
+function scrollWindows(direction) {
+    const container = document.getElementById("scrollContainer");
+    const scrollAmount = 260;
+    container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
